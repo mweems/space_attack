9066a1412278c2be63d02cb4467ae76b753b619c
Matt Weems <mj_weems@yahoo.com>
2013-02-16 21:17:55 -0600

    added images, modified front end

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,6 +7,7 @@ class GameController < ApplicationController
 		player_2_ships = params[:player_2_ships].split("\r\n")
 		player_1_fleet = FleetBuilder.build(player_1_ships)
 		player_2_fleet = FleetBuilder.build(player_2_ships)
+		Rails.logger.info("player 1 fleet: #{player_1_fleet.inspect}")
 
 		@result = GameEngine.fight(player_1_fleet, player_2_fleet)
 	end
