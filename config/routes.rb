5e9e8fe35b3dc71eb46676d172cf5453fa1d4857
Matt Weems <mj_weems@yahoo.com>
2013-02-16 21:40:50 -0600

    Set default route to point to game#new

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ SpaceAttack::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
+  match '/' => 'game#new'
   match ':controller(/:action(/:id))(.:format)'
   #/game/new
 end
